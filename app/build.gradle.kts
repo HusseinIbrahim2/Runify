bca9252d8d630138a648072e1e3d6af3c1fa0c30
Hussein Ibrahim <husseinib23920@gmail.com>
2024-11-01 23:31:51 +0100

    Create map layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation ("com.google.android.gms:play-services-maps:18.0.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
