dc673e2694b011e78c39d2e944dbc9c16c823f96
Hussein Ibrahim <husseinib23920@gmail.com>
2024-11-02 23:14:01 +0100

    Setup step counter sensor

--- app/src/main/java/com/example/runify/MainActivity.java
+++ app/src/main/java/com/example/runify/MainActivity.java
@@ -1,17 +1,50 @@
 package com.example.runify;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.runify.databinding.ActivityMainBinding;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
+
+    private TextView stepCountText, distanceText,
+            caloriesText, speedText, elevationText;
+
+    private MaterialCardView statsCard;
+    private FloatingActionButton resetFab;
+    private Button shareButton, startTrackingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+    }
+
+    private void initializeViews() {
+        stepCountText = findViewById(R.id.stepCountText);
+        distanceText = findViewById(R.id.distanceText);
+        caloriesText = findViewById(R.id.caloriesText);
+        speedText = findViewById(R.id.speedText);
+        elevationText = findViewById(R.id.elevationText);
+        statsCard = findViewById(R.id.statsCard);
+        resetFab = findViewById(R.id.resetFab);
+        shareButton = findViewById(R.id.shareButton);
+        startTrackingButton = findViewById(R.id.startTrackingButton);
+    }
+
+    private void setupSensors() {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
     }
 }
+
